70eb9dda54896d560d33a6e9bd6d07d173e40934
Yahiro Teraoka <yteraoka@lcs.on.ca>
2026-02-09 14:13:09 -0500

    Initial Commit

---
+++ GeometricFigures/GeometricFiguresApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometricFiguresApp.swift
+//  GeometricFigures
+//
+//  Created by Yahiro Teraoka on 2026-02-09.
+//
+
+import SwiftUI
+
+@main
+struct GeometricFiguresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
